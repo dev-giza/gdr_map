27eefad9629eb305e7aad76eff881a4e0ff25cd4
gi3a <gizat.uteev@gmail.com>
2025-05-30 22:58:37 -0500

    v0.2

--- server/api/clients-with-coords.ts
+++ server/api/clients-with-coords.ts
@@ -2,7 +2,7 @@ import { readFile } from "fs/promises";
 
 export default defineEventHandler(async (event) => {
   try {
-    const data = await readFile("clients_with_coords.json", "utf-8");
+    const data = await readFile("server/clients_with_coords.json", "utf-8");
     return JSON.parse(data);
   } catch {
     return [];
